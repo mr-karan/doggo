25d277577c157ad8b0265ee779e8c979a14c8bd1
Karan Sharma <hello@mrkaran.dev>
2021-03-04 20:34:54 +0530

    feat: purge unused css in tailwind

---
+++ cmd/doggo/api/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './index.html',
+    './assets/main.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
