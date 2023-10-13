3bc35eaff4f47d6c3679204df977f584b112d9c2
molikuner <molikuner@gmail.com>
2023-10-13 06:31:13 +0200

    Fix syntax error in ZSH completion
    GH-60

--- completions/doggo.zsh
+++ completions/doggo.zsh
@@ -1,6 +1,6 @@
-#compdef doggo
+#compdef _doggo doggo
 
-__doggo() {
+_doggo() {
     _arguments \
         "(- 1 *)"{-v,--version}"[Show version of doggo]" \
         "(- 1 *)"{-\?,--help}"[Show list of command-line options]" \
@@ -9,10 +9,8 @@ __doggo() {
         {-n,--nameserver}"[Address of a specific nameserver to send queries to]::_hosts;" \
         {-c,--class}"[Network class of the DNS record being queried]:(network class):(IN CH HS)" \
         {-J,--json}"[Format the output as JSON]" \
-        {--color}"[Defaults to true. Set --color=false to disable colored output]:(setting):(true false)" \
-        {--debug}"[Enable debug logging]:(setting):(true false)" \
-        --time"[Shows how long the response took from the server"] \
-        '*:filename:_hosts'
+        --color"[Defaults to true. Set --color=false to disable colored output]:(setting):(true false)" \
+        --debug"[Enable debug logging]:(setting):(true false)" \
+        --time"[Shows how long the response took from the server]" \
+        '*:hostname:_hosts'
 }
-
-__doggo
